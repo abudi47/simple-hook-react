434250ce2a7b694f5ab7673a0fa09abf02571592
Abudi <abduselamt47@gmail.com>
2024-12-14 06:47:30 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+
+import { useState } from 'react';
+import './index.css'
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+function App() {
+  const [step, setStep] = useState(1);
+  
+  function prevHandler() {
+    if (step > 1) setStep(step-1);
+    
+  }
+  function nextHandler() {
+   if (step < 3) setStep(step+1);
+  }
+
+
+
+
+  return (
+   <>
+      <div className='steps'>
+        <div className='numbers'>
+          <div className={step>=1 ? "active" : ""}>1</div>
+          <div className={step>=2 ? "active" : ""}>2</div>
+          <div className={step>=3 ? "active" : ""}>3</div>
+        </div>
+
+        <p className='message'>Step {step} : {messages[step-1]} </p>
+        <div className='buttons'>
+          <button style={{ backgroundColor: "#7950f2" , color:"#fff" }} onClick={prevHandler} > Previous</button>
+          <button style={{ backgroundColor: "#7950f2" , color:"#fff" }} onClick={nextHandler}> Next</button>
+
+        </div>
+      </div>
+   </>
+  )
+}
+
+export default App
